f515a1c39f75ee3cc830a913335bc796079a7dc8
Shaban Najaat <najaatmohammedshaban@gmail.com>
2025-09-02 04:01:48 +0100

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'relationship_app'
+
 urlpatterns = [
+    # Function-based view route
     path('books/', views.list_books, name='list_books'),
-    path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    # Class-based view route
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]

805ea81ba6c36b4b8bd64d7619e2d3f6970ba847
Shaban Najaat <najaatmohammedshaban@gmail.com>
2025-09-02 04:04:34 +0100

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,14 +1,33 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library  # MUST have this exact import line
+from .models import Book, Library
 
-# Function-based view that renders a simple text list of book titles and their authors
+# Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+    # For plain text response instead of HTML template
+    book_list = []
+    for book in books:
+        book_list.append(f"{book.title} by {book.author.name}")
+    
+    return render(request, 'relationship_app/list_books.html', {
+        'books': books,
+        'book_list_text': "\n".join(book_list)
+    })
 
-# Class-based view that displays details for a specific library, listing all books available
-class LibraryDetailView(DetailView):  # MUST use DetailView
-    model = Library  # MUST specify the model
+# Class-based view to display library details
+class LibraryDetailView(DetailView):
+    model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        library = self.get_object()
+        # For plain text response
+        book_list = []
+        for book in library.books.all():
+            book_list.append(f"{book.title} by {book.author.name} (Published {book.publication_year})")
+        
+        context['book_list_text'] = "\n".join(book_list)
+        return context

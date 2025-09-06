186e4f82a61397ff950df044cd9062d5c2ca6132
Shaban Najaat <najaatmohammedshaban@gmail.com>
2025-09-06 23:23:33 +0100

    Update models.py

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Library(models.Model):
     name = models.CharField(max_length=100)
-    books = models.ManyToManyField('Book')
+    books = models.ManyToManyField('Book', related_name='libraries')
     
     def _str_(self):
         return self.name

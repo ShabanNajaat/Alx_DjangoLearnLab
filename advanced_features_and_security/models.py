905cc7b004afe1fa5c03c0bacc5b63e4d66c21c6
Shaban Najaat <najaatmohammedshaban@gmail.com>
2025-09-14 20:48:05 +0100

    Create models.py

---
+++ advanced_features_and_security/models.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import AbstractUser, UserManager
+from django.db import models
+
+class CustomUserManager(UserManager):
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(username, email, password, **extra_fields)
+
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self._create_user(username, email, password, **extra_fields)
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    objects = CustomUserManager()

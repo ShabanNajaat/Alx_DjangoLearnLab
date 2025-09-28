33eea3be83ac30c6f079ead9c17cc6445e22e0bd
Shaban Najaat <najaatmohammedshaban@gmail.com>
2025-09-28 08:57:07 +0000

    Create settings.py

---
+++ advanced-api-project/settings.py
@@ -0,0 +1,10 @@
+# Add to Django REST Framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ]
+}

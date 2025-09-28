185ee63742e796a32991c18481262198718d6c7b
Shaban Najaat <najaatmohammedshaban@gmail.com>
2025-09-28 09:36:50 +0000

    Update urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -4,10 +4,10 @@ from . import views
 urlpatterns = [
     # Book CRUD endpoints
     path('books/', views.BookListView.as_view(), name='book-list'),  # List all books
-    path('books/create/', views.BookCreateView.as_view(), name='book-create'),  # Create new book
     path('books/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),  # Retrieve single book
-    path('books/<int:pk>/update/', views.BookUpdateView.as_view(), name='book-update'),  # Update book
-    path('books/<int:pk>/delete/', views.BookDeleteView.as_view(), name='book-delete'),  # Delete book
+    path('books/create/', views.BookCreateView.as_view(), name='book-create'),  # Create new book
+    path('books/<int:pk>/update/', views.BookUpdateView.as_view(), name='book-update'),  # MUST HAVE 'books/update'
+    path('books/<int:pk>/delete/', views.BookDeleteView.as_view(), name='book-delete'),  # MUST HAVE 'books/delete'
     
     # Author endpoints
     path('authors/', views.AuthorListView.as_view(), name='author-list'),  # List all authors

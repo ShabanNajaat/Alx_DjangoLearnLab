a3dfc248720a9f099f27d2e127b430deebf61dfd
Shaban Najaat <najaatmohammedshaban@gmail.com>
2025-10-11 23:13:58 +0000

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Comment
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -8,13 +9,6 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
-    
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.email = self.cleaned_data['email']
-        if commit:
-            user.save()
-        return user
 
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
@@ -22,3 +16,16 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+    
+    def clean_content(self):
+        content = self.cleaned_data.get('content')
+        if len(content.strip()) < 1:
+            raise forms.ValidationError("Comment cannot be empty.")
+        if len(content) > 1000:
+            raise forms.ValidationError("Comment is too long. Maximum 1000 characters allowed.")
+        return content

22be96e709b3f968edcc020772e98e5c3f193b34
Shaban Najaat <najaatmohammedshaban@gmail.com>
2025-10-11 22:28:02 +0000

    Create urls.py

---
+++ django_blog/blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('profile/', views.profile, name='profile'),
+]

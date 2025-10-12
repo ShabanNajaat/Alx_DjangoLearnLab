ff24d43c877e4deef253fb70dceab5dd3f46c548
Shaban Najaat <najaatmohammedshaban@gmail.com>
2025-10-12 13:05:23 +0000

    Update serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -5,53 +5,34 @@ from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'email', 'bio', 'profile_picture', 'followers')
-        read_only_fields = ('id', 'followers')
-
-class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, min_length=6)
-    password_confirm = serializers.CharField(write_only=True, min_length=6)
+class UserProfileSerializer(serializers.ModelSerializer):
+    following_count = serializers.SerializerMethodField()
+    followers_count = serializers.SerializerMethodField()
+    is_following = serializers.SerializerMethodField()
     
     class Meta:
         model = User
-        fields = ('username', 'email', 'password', 'password_confirm', 'bio', 'profile_picture')
+        fields = ('id', 'username', 'email', 'bio', 'profile_picture', 
+                 'following_count', 'followers_count', 'is_following')
+        read_only_fields = ('id', 'following_count', 'followers_count', 'is_following')
     
-    def validate(self, attrs):
-        if attrs['password'] != attrs['password_confirm']:
-            raise serializers.ValidationError({"password": "Password fields didn't match."})
-        return attrs
+    def get_following_count(self, obj):
+        return obj.get_following_count()
     
-    def create(self, validated_data):
-        validated_data.pop('password_confirm')
-        # Use the exact required syntax
-        user = get_user_model().objects.create_user(
-            username=validated_data['username'],
-            email=validated_data.get('email', ''),
-            password=validated_data['password'],
-            bio=validated_data.get('bio', ''),
-            profile_picture=validated_data.get('profile_picture', None)
-        )
-        # Create token for the user
-        Token.objects.create(user=user)
-        return user
+    def get_followers_count(self, obj):
+        return obj.get_followers_count()
+    
+    def get_is_following(self, obj):
+        request = self.context.get('request')
+        if request and request.user.is_authenticated:
+            return request.user.is_following(obj)
+        return False
 
-class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField()
-    
-    def validate(self, attrs):
-        username = attrs.get('username')
-        password = attrs.get('password')
-        
-        if username and password:
-            user = authenticate(username=username, password=password)
-            if not user:
-                raise serializers.ValidationError('Unable to log in with provided credentials.')
-            attrs['user'] = user
-        else:
-            raise serializers.ValidationError('Must include "username" and "password".')
-        
-        return attrs
+# Keep your existing serializers and add these:
+class FollowSerializer(serializers.Serializer):
+    user_id = serializers.IntegerField()
+
+class UserSearchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'bio', 'profile_picture')

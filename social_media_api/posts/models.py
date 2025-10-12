e93fbc65da69bc5aa1733477cdc592d8d9bf98fd
Shaban Najaat <najaatmohammedshaban@gmail.com>
2025-10-12 18:19:19 +0000

    Update models.py

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 User = get_user_model()
 
@@ -15,6 +17,12 @@ class Post(models.Model):
     
     def __str__(self):
         return f"{self.title} by {self.author.username}"
+    
+    def get_likes_count(self):
+        return self.likes.count()
+    
+    def is_liked_by(self, user):
+        return self.likes.filter(user=user).exists()
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
@@ -28,3 +36,15 @@ class Comment(models.Model):
     
     def __str__(self):
         return f"Comment by {self.author.username} on {self.post.title}"
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        unique_together = ['user', 'post']
+        ordering = ['-created_at']
+    
+    def __str__(self):
+        return f"{self.user.username} likes {self.post.title}"

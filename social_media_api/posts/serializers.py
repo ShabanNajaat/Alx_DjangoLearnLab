c68f35000b06153dd142aa24c5474bf7ffedb7b1
Shaban Najaat <najaatmohammedshaban@gmail.com>
2025-10-12 12:41:44 +0000

    Create serializers.py

---
+++ social_media_api/posts/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+from .models import Post, Comment
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email')
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+    
+    class Meta:
+        model = Comment
+        fields = ('id', 'post', 'author', 'content', 'created_at', 'updated_at')
+        read_only_fields = ('id', 'author', 'created_at', 'updated_at')
+
+class PostSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+    comments = CommentSerializer(many=True, read_only=True)
+    comments_count = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Post
+        fields = ('id', 'author', 'title', 'content', 'created_at', 'updated_at', 'comments', 'comments_count')
+        read_only_fields = ('id', 'author', 'created_at', 'updated_at', 'comments')
+    
+    def get_comments_count(self, obj):
+        return obj.comments.count()
+
+class PostCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ('title', 'content')
+
+class CommentCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ('content',)

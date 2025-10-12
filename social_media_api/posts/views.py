34efeef8833d68a8346dfae6327aa3124febec2e
Shaban Najaat <najaatmohammedshaban@gmail.com>
2025-10-12 12:43:38 +0000

    Create views.py

---
+++ social_media_api/posts/views.py
@@ -0,0 +1,55 @@
+from rest_framework import viewsets, status, filters
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Post, Comment
+from .serializers import (
+    PostSerializer, PostCreateSerializer, 
+    CommentSerializer, CommentCreateSerializer
+)
+from .permissions import IsAuthorOrReadOnly
+
+class PostViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['title', 'content']
+    filterset_fields = ['author']
+    ordering_fields = ['created_at', 'updated_at']
+    ordering = ['-created_at']
+    
+    def get_queryset(self):
+        return Post.objects.select_related('author').prefetch_related('comments__author')
+    
+    def get_serializer_class(self):
+        if self.action in ['create', 'update', 'partial_update']:
+            return PostCreateSerializer
+        return PostSerializer
+    
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+    
+    @action(detail=True, methods=['post'], permission_classes=[IsAuthenticatedOrReadOnly])
+    def add_comment(self, request, pk=None):
+        post = self.get_object()
+        serializer = CommentCreateSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            comment = serializer.save(post=post, author=request.user)
+            return Response(CommentSerializer(comment).data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    serializer_class = CommentSerializer
+    
+    def get_queryset(self):
+        return Comment.objects.select_related('author', 'post')
+    
+    def get_serializer_class(self):
+        if self.action in ['create', 'update', 'partial_update']:
+            return CommentCreateSerializer
+        return CommentSerializer
+    
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)

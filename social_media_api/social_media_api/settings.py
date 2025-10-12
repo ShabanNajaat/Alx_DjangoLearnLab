823b41fddf09e60f1e2daf053e3b0d916461b9e3
Shaban Najaat <najaatmohammedshaban@gmail.com>
2025-10-12 12:15:17 +0000

    Create settings.py

---
+++ social_media_api/social_media_api/settings.py
@@ -0,0 +1,19 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'accounts',
+]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
+
+AUTH_USER_MODEL = 'accounts.CustomUser'

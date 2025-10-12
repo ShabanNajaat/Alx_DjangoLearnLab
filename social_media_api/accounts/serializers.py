67cf49dd08d9c2b11c741db978dca4de7f69d872
Shaban Najaat <najaatmohammedshaban@gmail.com>
2025-10-12 12:28:51 +0000

    Update serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,28 +1,56 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
 from django.contrib.auth import authenticate
-from .models import CustomUser
+from rest_framework.authtoken.models import Token
+
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CustomUser
+        model = User
         fields = ('id', 'username', 'email', 'bio', 'profile_picture', 'followers')
+        read_only_fields = ('id', 'followers')
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True, min_length=6)
+    password_confirm = serializers.CharField(write_only=True, min_length=6)
     
     class Meta:
-        model = CustomUser
-        fields = ('username', 'email', 'password', 'bio')
+        model = User
+        fields = ('username', 'email', 'password', 'password_confirm', 'bio', 'profile_picture')
+    
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password_confirm']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        return attrs
     
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(
+        validated_data.pop('password_confirm')
+        user = User.objects.create_user(
             username=validated_data['username'],
-            email=validated_data['email'],
+            email=validated_data.get('email', ''),
             password=validated_data['password'],
-            bio=validated_data.get('bio', '')
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture', None)
         )
+        # Create token for the user
+        Token.objects.create(user=user)
         return user
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField()
+    
+    def validate(self, attrs):
+        username = attrs.get('username')
+        password = attrs.get('password')
+        
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if not user:
+                raise serializers.ValidationError('Unable to log in with provided credentials.')
+            attrs['user'] = user
+        else:
+            raise serializers.ValidationError('Must include "username" and "password".')
+        
+        return attrs
